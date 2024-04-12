6b40b6371e62fe711285decbcc523bb514dc0766
unknown <ruslanchik_nadyrov@mail.ru>
2024-04-12 19:13:57 +0600

    Updates

--- _nuxt/redirect.DNeJChpg.js
+++
@@ -1,1 +0,0 @@
-import{a6 as t,q as o}from"./entry.DX0wL5kp.js";import{u as r}from"./cookie.CQm0Ow4G.js";const m=t((a,e)=>r("token").value&&e.path=="/"?o("/home"):null);export{m as default};
---
+++ _nuxt/redirect.ulobwMmF.js
@@ -0,0 +1,1 @@
+import{a6 as t,q as o}from"./entry.DJLNJNwO.js";import{u as r}from"./cookie.CgwEJbi6.js";const m=t((a,e)=>r("token").value&&e.path=="/"?o("/home"):null);export{m as default};
